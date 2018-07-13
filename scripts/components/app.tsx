dd71354d382fa4d29586960c24961991a77f6655
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-07-12 20:23:53 -0700

    initial commit

---
+++ scripts/components/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Label } from "./label";
+
+export class App extends React.Component {
+    render() {
+        return (
+            <Label />
+        );
+    }
+}
---
+++ scripts/components/label.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export class Label extends React.Component {
+    render(): JSX.Element {
+        return (
+            <div className="label">
+                Pictures made of 100% organic, locally sourced,
+                farm to table, custom, hand written code.
+            </div>
+        );
+    }
+}
